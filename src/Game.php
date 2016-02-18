7cb3210450b02046b051f107dd5e2a95d3a33c38
Jon Sweeney <jonsweeney26@gmail.com>
2016-02-18 09:15:57 -0800

    add the logic for computer

--- src/Game.php
+++ src/Game.php
@@ -4,14 +4,36 @@
      **/
     class Game
     {
-        //Game
+        //playGame takes in the input hand 1 and 2
         function playGame($hand_one, $hand_two)
         {
+            //computer
+            if($hand_two == 'Computer') {
+                $computer = rand(1,3);
+                if($computer == 1) {
+                    $hand_two = 'rock';
+                } elseif ($computer == 2) {
+                    $hand_two = 'scissors';
+                } else {
+                    $hand_two = 'paper';
+                }
+            }
+            //game
             if($hand_one == $hand_two) {
                 return 'Tie';
             }
             if($hand_one == 'rock') {
                 if($hand_two == 'scissors') {
+                    return 'Player 1';
+                }
+                return 'Player 2';
+            } elseif($hand_one == 'scissors') {
+                if($hand_two == 'paper') {
+                    return 'Player 1';
+                }
+                return 'Player 2';
+            } elseif($hand_one == 'paper') {
+                if($hand_two == 'rock') {
                     return 'Player 1';
                 }
                 return 'Player 2';
